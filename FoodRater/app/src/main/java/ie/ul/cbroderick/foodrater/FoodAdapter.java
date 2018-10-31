65abb2074c6d96e5fc365e8a3acce5569fff2e45
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-31 10:36:14 +0000

    Food Item View plus code stubs

---
+++ FoodRater/app/src/main/java/ie/ul/cbroderick/foodrater/FoodAdapter.java
@@ -0,0 +1,45 @@
+package ie.ul.cbroderick.foodrater;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public abstract class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder>{
+
+    public void addFood(){
+
+    }
+
+    @NonNull
+    @Override
+    public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class FoodViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mImageView;
+        private TextView mName;
+        private RatingBar mRatingBar;
+
+        public FoodViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.food_pic);
+            mName = itemView.findViewById(R.id.name);
+            mRatingBar = itemView.findViewById(R.id.rating_bar);
+        }
+    }
+}
