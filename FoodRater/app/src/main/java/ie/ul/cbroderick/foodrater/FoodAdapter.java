fbf910a3b9d3ece38b2d64a8c09b5ace31ac5d8d
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-31 11:08:01 +0000

    Updated to end of videos with TODOs

--- FoodRater/app/src/main/java/ie/ul/cbroderick/foodrater/FoodAdapter.java
+++ FoodRater/app/src/main/java/ie/ul/cbroderick/foodrater/FoodAdapter.java
@@ -8,25 +8,39 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder>{
 
+    private List<Food> mFoods = new ArrayList<>();
+
     public void addFood(){
+        // TODO
+
 
     }
 
     @NonNull
     @Override
     public FoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // TODO
+
+
         return null;
     }
 
     @Override
     public void onBindViewHolder(@NonNull FoodViewHolder holder, int position) {
-
+        final Food food = mFoods.get(position);
+        holder.mName.setText(food.getName());
+        holder.mImageView.setImageResource(food.getImageResourceId());
+        holder.mRatingBar.setRating(food.getRating());
     }
 
     @Override
     public int getItemCount() {
+        // TODO
         return 0;
     }
 
@@ -40,6 +54,21 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
             mImageView = itemView.findViewById(R.id.food_pic);
             mName = itemView.findViewById(R.id.name);
             mRatingBar = itemView.findViewById(R.id.rating_bar);
+
+            //TODO: Delete this food on long press
+
+            // Done together, update the rating for this food
+            mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    if (fromUser) {
+                        // Update this food's rating
+                        Food currentFood = mFoods.get(getAdapterPosition());
+                        currentFood.setRating(rating);
+                    }
+
+                }
+            });
         }
     }
 }
